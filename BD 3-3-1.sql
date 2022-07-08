03bb5792c782a62ee4c24f9860e72fd7ca5edb03
Sergww <103686356+Sergww@users.noreply.github.com>
2022-07-09 00:45:47 +0300

    Update BD 3-3-1.sql

--- BD 3-3-1.sql
+++ BD 3-3-1.sql
@@ -17,7 +17,6 @@ INSERT INTO Genres(name)
 VALUES('chanson');
 
 
-
 INSERT INTO musicians (nickname) 
 VALUES('Roxette');
 
@@ -31,7 +30,7 @@ INSERT INTO musicians (nickname)
 VALUES('Louis Armstrong');
 
 INSERT INTO musicians (nickname)  
-VALUES('Круг');
+VALUES('РљСЂСѓРі');
 
 INSERT INTO musicians (nickname)  
 VALUES('Madonna');
@@ -41,7 +40,6 @@ VALUES('Edith Piaf');
 
 INSERT INTO musicians (nickname) 
 VALUES('Elvis Presley');
-
 
 
 INSERT INTO GenreMusician (musician_id, genre_id) 
@@ -75,7 +73,6 @@ INSERT INTO GenreMusician (musician_id, genre_id)
 VALUES(8, 2);
 
 
-
 INSERT INTO albums (title, year_release) 
 VALUES('Good Karma', 2018);
 
@@ -86,16 +83,16 @@ INSERT INTO albums (title, year_release)
 VALUES('Help!', 1965);
 
 INSERT INTO albums (title, year_release) 
-VALUES('Красота и Уродство', 2021);
+VALUES('РљСЂР°СЃРѕС‚Р° Рё РЈСЂРѕРґСЃС‚РІРѕ', 2021);
 
 INSERT INTO albums (title, year_release) 
 VALUES('Louis Armstrong and His Friends', 1971);
 
 INSERT INTO albums (title, year_release)  
-VALUES('Мадам', 1998);
+VALUES('РњР°РґР°Рј', 1998);
 
 INSERT INTO albums (title, year_release) 
-VALUES('Зеленый прокурор (Светочка)', 1996);
+VALUES('Р—РµР»РµРЅС‹Р№ РїСЂРѕРєСѓСЂРѕСЂ (РЎРІРµС‚РѕС‡РєР°)', 1996);
 
 INSERT INTO albums (title, year_release) 
 VALUES('Erotica', 1992);
@@ -132,11 +129,6 @@ INSERT INTO MusicianAlbum (musician_id, album_id)
 VALUES(7, 9);
 
 
-
-
-
-
-
 INSERT INTO Compilation (title, year_release)
 values ('Best', 2018);
 
@@ -147,16 +139,16 @@ INSERT INTO Compilation (title, year_release)
 values ('Anthology 1', 1995);
 
 INSERT INTO Compilation (title, year_release)
-values ('Смутное время', 2022);
+values ('РЎРјСѓС‚РЅРѕРµ РІСЂРµРјСЏ, 2022);
 
 INSERT INTO Compilation (title, year_release)
 values ('The Complete Louis Armstrong Columbia', 2021);
 
 INSERT INTO Compilation (title, year_release)  
-values ('Легенды русского шансона. Том 1', 1999);  
+values ('Р›РµРіРµРЅРґС‹ СЂСѓСЃСЃРєРѕРіРѕ С€Р°РЅСЃРѕРЅР°. РўРѕРј 1', 1999);  
 
 INSERT INTO Compilation (title, year_release)
-values ('Ветер северный. Сборник русского шансона', 2009);
+values ('Р’РµС‚РµСЂ СЃРµРІРµСЂРЅС‹Р№. РЎР±РѕСЂРЅРёРє СЂСѓСЃСЃРєРѕРіРѕ С€Р°РЅСЃРѕРЅР°', 2009);
 
 INSERT INTO Compilation (title, year_release)
 values ('The Immaculate Collection', 1990);
@@ -188,10 +180,10 @@ INSERT INTO Track (title, duration, album_id)
 values ('Yesterday', 123, 3);
 
 INSERT INTO Track (title, duration, album_id)
-values ('Мы все умрём', 166, 4);
+values ('РњС‹ РІСЃРµ СѓРјСЂС‘Рј', 166, 4);
 
 INSERT INTO Track (title, duration, album_id)
-values ('Агент', 215, 4);
+values ('ГЂГЈГҐГ­ГІ', 215, 4);
 
 INSERT INTO Track (title, duration, album_id)
 values ('My One and Only Love', 199, 5);
@@ -200,10 +192,10 @@ INSERT INTO Track (title, duration, album_id)
 values ('Boy from New Orleans', 237, 5);
 
 INSERT INTO Track (title, duration, album_id)
-values ('Владимирский централ', 267, 6);
+values ('Р’Р»Р°РґРёРјРёСЂСЃРєРёР№ С†РµРЅС‚СЂР°Р»', 267, 6);
 
 INSERT INTO Track (title, duration, album_id)
-values ('Милый мой город', 235, 7);
+values ('РњРёР»С‹Р№ РјРѕР№ РіРѕСЂРѕРґ', 235, 7);
 
 INSERT INTO Track (title, duration, album_id)
 values ('Erotica', 270, 8);
