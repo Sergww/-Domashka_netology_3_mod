0837262282a696da4cd57c07f5e0c29aa31aa8c6
Sergww <103686356+Sergww@users.noreply.github.com>
2022-07-10 16:33:54 +0300

    Update BD 3-3-2.sql

--- BD 3-3-2.sql
+++ BD 3-3-2.sql
@@ -1,6 +1,8 @@
 SELECT title, year_release FROM Albums
 WHERE year_release = 2018;
 
+SELECT MAX(duration) FROM Track;
+
 SELECT title, duration FROM Track
 ORDER BY duration DESC
 LIMIT 1;
@@ -12,7 +14,13 @@ SELECT title FROM Compilation
 WHERE year_release BETWEEN 2018 AND 2020;
 
 SELECT title FROM Track
-WHERE title ILIKE '%my%' OR title LIKE 'мой';
+WHERE title ILIKE '%my%' OR title ILIKE 'мой';
 
 SELECT nickname FROM Musicians
 WHERE (length(nickname)-length(replace(nickname,' ',''))+1) = 1
+
+SELECT nickname FROM Musicians
+WHERE array_length(regexp_split_to_array(nickname, '\s+'), 1) = 1;
+
+SELECT nickname FROM Musicians
+WHERE array_length(string_to_array(nickname, ' '), 1) = 1;
